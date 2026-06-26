64a5e9a4472c6f1b29793a80616c46189cd6449d
MehtaNandini <153070084+MehtaNandini@users.noreply.github.com>
2026-06-26 15:02:25 +0200

    feat: refactor frontend architecture to implement react-router navigation and modularize components

--- vehicle-anomaly-platform/frontend/src/main.tsx
+++ vehicle-anomaly-platform/frontend/src/main.tsx
@@ -1,10 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter } from 'react-router-dom'
+import { TelemetryProvider } from './context/TelemetryContext'
 import App from './App.tsx'
 import './index.css'
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
   <React.StrictMode>
-    <App />
+    <BrowserRouter>
+      <TelemetryProvider>
+        <App />
+      </TelemetryProvider>
+    </BrowserRouter>
   </React.StrictMode>,
 )
